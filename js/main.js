ea6ea97cadfed8dc7e839d10cc75352b548e36e2
greenshark-ben <benjamin@greenshark.com>
2017-12-13 15:06:38 -0600

    primera etapa boton muestra grafica

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+console.log("hola mundo desde el main")
+
+// hide -show
+$('#s_chart_1').click(function(){
+   $('#chart_show').toggle();
+});
+
+$('#s_chart_2').click(function(){
+   $('#chart_show_2').toggle();
+});
+
+$('#s_chart_3').click(function(){
+   $('#chart_show_3').toggle();
+});
